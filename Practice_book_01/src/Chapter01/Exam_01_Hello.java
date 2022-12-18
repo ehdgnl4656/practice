52fa017764f817ec7895845e35bf0a40a6fd0224
PDH <ehdgnl4656@naver.com>
2022-12-19 08:26:46 +0900

    init

---
+++ Practice_book_01/src/Chapter01/Exam_01_Hello.java
@@ -0,0 +1,8 @@
+package Chapter01;
+
+public class Exam_01_Hello {
+	public static void main(String[] args) {
+		System.out.println("Hello, welcome to the java world!");
+	}
+
+}
---
+++ Practice_book_01/src/Chapter01/Exam_02_Hello.java
@@ -0,0 +1,10 @@
+package Chapter01;
+
+public class Exam_02_Hello {
+	public static void main(String[] args) {
+		int x = 1;
+		int y = 2;
+		int result = x + y;
+		System.out.println(x + " + " + y + " = " + result);
+	}
+}
